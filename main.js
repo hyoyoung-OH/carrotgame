e6661ed323e5fa68048d61cc3d9a4994ed0ac215
hyoyoung-OH <lisa4397@gmail.com>
2022-03-23 03:40:08 +0900

    게임,타이머 시작

--- main.js
+++ main.js
@@ -1,13 +1,72 @@
 "use strict";
+
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect(); //field의 전체적인 사이즈와 포지션을 알 수 있다
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
 
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minute = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minute}:${seconds}`;
+}
 function initGame() {
+  field.innerHTML = "";
+  gameScore.innerHTML = CARROT_COUNT;
   // 벌레와 당근을 생성한 뒤 field에 추가해줌
-  console.log(fieldRect);
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
+  addItem("carrot", CARROT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
 }
 
 function addItem(className, count, imgPath) {
@@ -31,4 +90,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-initGame();
